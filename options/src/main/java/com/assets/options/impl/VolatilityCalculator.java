3bdde3fd548ca126fcee811744a845f269bfe1fa
Javier Molina <javier.molina@payworks.com>
2017-09-15 20:24:02 +0200

    Cleaning house

--- options/src/main/java/com/assets/options/impl/VolatilityCalculator.java
+++ options/src/main/java/com/assets/options/impl/VolatilityCalculator.java
@@ -1,6 +1,7 @@
 package com.assets.options.impl;
 
 import com.assets.entities.Candlestick;
+import com.assets.statistic.list.StockList;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 
 import java.util.List;
@@ -9,13 +10,21 @@ public class VolatilityCalculator {
 
     private static final double DAYS_OF_YEAR = 252d;
 
-    public double getAnnualizedVolatility(List<Candlestick> history) {
-        StandardDeviation standardDeviation = new StandardDeviation();
+    public double getAnnualizedVolatility(StockList history) {
+        return getVolatility(history.stream().mapToDouble(x -> x.getValue().doubleValue()).toArray());
+    }
 
+    public double getAnnualizedVolatility(List<Candlestick> history) {
         double[] endValues = history
                 .stream()
                 .mapToDouble(candlestick -> candlestick.getFinalPrice().doubleValue())
                 .toArray();
+
+        return getVolatility(endValues);
+    }
+
+    private double getVolatility(double[] endValues) {
+        StandardDeviation standardDeviation = new StandardDeviation();
 
         double[] percentDifferences = new double[endValues.length - 1];
         for (int i = 1; i < endValues.length; i++) {
